4a5e73f8f2596b4c28c5f99aa0cb21c418eb9c3c
LambosLeague <hameed_lambo@yahoo.com>
2021-02-05 06:32:15 +0000

    Works Started

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 30; num <= 39; num++)
+	{
+	putchar(num);
+	}
+	putchar(10);
+	return (0);
+}
