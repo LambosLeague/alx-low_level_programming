af083a60cc86bb9415f9b5d6edc950bfa6633048
LambosLeague <hameed_lambo@yahoo.com>
2021-02-13 06:23:04 +0000

    Testing

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,15 +1,11 @@
 #include "holberton.h"
 
-/* _putchar -prints to screen
+/* 
  * main -Entry point
  *
  * return -success
  */
 
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 int main(void)
 {
 	char hol_school[] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
