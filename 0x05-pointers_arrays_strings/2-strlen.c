4fb7c74b90f632db2a327112b8bdb6208bc40d95
LambosLeague <hameed_lambo@yahoo.com>
2021-02-16 00:00:57 +0000

    upto task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _strlen - counts length of a sting or char pointer
+ * @s: accept user input
+ * Return: always return legnth counted
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+	count++;
+	s++;
+	}
+	return (count);
+}
