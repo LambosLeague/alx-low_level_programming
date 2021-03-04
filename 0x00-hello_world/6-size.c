2127c66a1a5e5e78b603fd33da683341f9c3bbe7
LambosLeague <hameed_lambo@yahoo.com>
2021-03-04 16:41:23 +0000

    fixed all bugs

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,8 @@
 	printf("Size of a char: %lu byte(s)\n", (long int)sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", (long int)sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", (long int)sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)\n", (long int)sizeof(long long int));
+	printf("Size of a long long int: %lu byte(s)\n", (long int)sizeof
+			(long long int));
 	printf("Size of a float: %lu byte(s)\n", (long int)sizeof(float));
 
 	return (0);
