e7ca8b33c4e87381f9b0f7f87e72ce8376d986cb
LambosLeague <hameed_lambo@yahoo.com>
2021-03-04 15:34:18 +0000

    completed all task in hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,11 @@
 #include <unistd.h>
 
+/**
+ * main - executes all your body of functions and classes
+ *
+ * Return: Always success return 0, but 1 if prompt with error
+ */
+
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
