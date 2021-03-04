9365ab06505e84cf159f876be89b6ed94d4f0d5a
LambosLeague <hameed_lambo@yahoo.com>
2021-03-04 08:28:27 +0000

    upto task 3 successfully done

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+
+/**
+ * _strdup - copy string and manipulate
+ * @str: buffers into allocated memory
+ * Return: returns string already manipulated
+ */
+
+char *_strdup(char *str)
+{
+	char *sstr;
+	unsigned int len = _strlen(str);
+
+	if (str == NULL || !str)
+	{
+		return (NULL);
+	}
+
+	sstr = malloc(sizeof(*str) * len);
+
+	unsigned int i = 0;
+
+	for (; i < len; i++)
+	{
+		*(sstr + i) = *(str + i);
+	}
+
+	return (sstr);
+}
+
+/**
+ * _strlen - count strings
+ * @s: - buffer s
+ * Return: length and count string
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+	count++;
+	s++;
+	}
+	return (count);
+}
