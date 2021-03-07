ff1890ad8f46d413496d9670e92c1893bd515d71
LambosLeague <hameed_lambo@yahoo.com>
2021-03-07 09:04:21 +0000

    completed all task and working

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -13,9 +13,14 @@ int main(void)
     int day;
     int year;
 
-    month = 2;
-    day = 29;
-    year = 2019;
+    printf("Enter Month: ");
+    scanf("%d", &month);
+
+    printf("Enter day: ");
+    scanf("%d", &day);
+    
+    printf("Enter Year: ");
+    scanf("%i", &year);
 
     printf("Date: %02d/%02d/%04d\n", month, day, year);
 
--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -19,8 +19,18 @@ void print_remaining_days(int month, int day, int year)
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 366 - day);
 		}
+		else
+		{
+			day++;
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 366 - day);
+		}
 	}
-	else if (month >= 2 && day > 31)
+	else if ((year % 4 == 1) && (month == 2) && day == 60)
+	{
+		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+	}
+	else if ((year % 4 == 1) && (month >= 2) && day > 31)
 	{
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 365 - day);
