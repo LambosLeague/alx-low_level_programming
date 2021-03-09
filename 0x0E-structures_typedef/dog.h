523a952eba02d480c5df7cbbc724cffda5efb33e
LambosLeague <hameed_lambo@yahoo.com>
2021-03-09 20:06:37 +0000

    task 0 - on structs

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef doggy
+#define doggy
+
+/**
+ * struct dog - dog is a pet, collecting its name and owner name with age
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owners name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* doggy */
