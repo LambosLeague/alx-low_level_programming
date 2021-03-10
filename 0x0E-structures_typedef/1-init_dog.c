1af3ffb4c07dc518f0da9ca40e38e9111288d276
LambosLeague <hameed_lambo@yahoo.com>
2021-03-10 08:16:23 +0000

    redone task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -4,13 +4,13 @@
 /**
  * init_dog - initializing into struct dog
  * @d: takes from struct dog
- * @name; name of dog
+ * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
-{	
+{
 	if (d)
 	{
 	(*d).name = name;
