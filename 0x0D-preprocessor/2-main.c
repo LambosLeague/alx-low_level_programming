dae5dea5ef9e0027af199a60ed3296e70e564776
LambosLeague <hameed_lambo@yahoo.com>
2021-03-10 07:59:10 +0000

    Task 2 completed

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,35 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - point to exceute all program
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: on succeess (0)
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * main - main program
+ *
+ * Return: 0 on success
  */
 
 int main(void)
 {
-	printf("%s\n", __FILE__);
+	char *file = __FILE__;
+
+	while (*file != '\0')
+	{
+		_putchar(*file);
+		file++;
+	}
+
+	_putchar(10);
+
+	return (0);
 }
