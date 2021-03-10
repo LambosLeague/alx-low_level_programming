7efa78c6d9067d28eb87efd049a712fe91fb3538
LambosLeague <hameed_lambo@yahoo.com>
2021-03-10 08:30:00 +0000

    Task 2 redone

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -18,10 +18,10 @@ void print_dog(struct dog *d)
 		printf("Name: %s\n", (*d).name);
 	}
 	if (d->age == 0)
-		printf("Age: (nil)\n");
+		printf("Age: %f\n", d->age);
 	else
 	{
-		printf("Age: %.1f\n", d->age);
+		printf("Age: %f\n", d->age);
 	}
 	if ((*d).owner == NULL)
 		printf("Owner: (nil)\n");
