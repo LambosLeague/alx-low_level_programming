1d36b6436c6ba368ec0584f31b6018518470fbaf
LambosLeague <hameed_lambo@yahoo.com>
2021-03-22 13:59:38 +0000

    task 0 completed

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "list.h"
+
+/**
+ * print_list - List is a structure containing some data and a pointer
+ * @h: this is a pointer
+ * Return: number of pointers
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int cnt = 2;
+	
+	list_t *ptr;
+	ptr = (*h).next;
+
+	while((*ptr).next)
+	{
+		cnt++;
+	}
+
+	if((*h).str == NULL && (*ptr).str == NULL)
+			printf("[0] (nil)\n");
+	else
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		printf("[%d] %s\n", ptr->len, ptr->str);
+	}
+
+
+	return (cnt);
+}
